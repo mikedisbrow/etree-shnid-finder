a56b7bd2cc017ffebab91ff52c08c5fe2a845695
Mike Disbrow <mikedisbrow@gmail.com>
2023-11-11 18:25:04 -0600

    make less verbose

--- shnid_finder.sh
+++ shnid_finder.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # shellcheck disable=SC1001
-set -euo pipefail
+# set -euo pipefail
 
 # requirements:
 # macOS     - use Homebrew (https://brew.sh)
@@ -9,14 +9,14 @@ set -euo pipefail
 # wget      = brew install wget
 # grep, tail, sed, awk are probably already available, but can also be
 # installed with `brew install grep tail sed awk`
-# 
-# This script can be run on a single specific file, or in the root of many 
+#
+# This script can be run on a single specific file, or in the root of many
 # directories containing FLAC files.
-# 
+#
 # $ `shnid_finder.sh somefile.flac` search etreedb.org for a matching shn ID#
 # and download the zip for any / all matches
-# 
-# $ `shnid_finder.sh` in a folder with many subdirectories will use the first 
+#
+# $ `shnid_finder.sh` in a folder with many subdirectories will use the first
 # FLAC file in each folder for the search and download the zip to each directory
 
 if [ $# -eq 0 ]
@@ -34,13 +34,14 @@ if [ $# -eq 0 ]
       result=$(wget -qO- https://etreedb.org/md5_lookup.php\?md5\="$fingerprint" \
         | grep -oE "\/shn_downloadzip\.php\?shnid\=[0-9]+")
       # print relatively unimportant info to stdout
-        echo file: "$track"
-        echo fingerprint: "$fingerprint"
-        echo shnid: "$shnid"
-        echo result: "$result"
+        # echo file: "$track"
+        # echo fingerprint: "$fingerprint"
+        # echo shnid: "$shnid"
+        # echo result: "$result"
+        echo downloading zip for "$shnid" to "$dir"
       for link in $result; do
         # assemble URL for each .zip and download them here
-        wget --content-disposition --trust-server-names https://etreedb.org"$link" -P "$dir"
+        wget -q --content-disposition --trust-server-names https://etreedb.org"$link" -P "$dir"
       done
     done
   else
@@ -54,13 +55,13 @@ if [ $# -eq 0 ]
     result=$(wget -qO- https://etreedb.org/md5_lookup.php\?md5\="$fingerprint" \
       | grep -oE "\/shn_downloadzip\.php\?shnid\=[0-9]+")
     # print relatively unimportant info to stdout
-      echo file: "$1"
-      echo fingerprint: "$fingerprint"
-      echo shnid: "$shnid"
-      echo result: "$result"
+      echo file: "$1" with fingerprint: "$fingerprint" matches shnid: "$shnid"
+      # echo fingerprint: "$fingerprint"
+      # echo shnid: "$shnid"
+      # echo result: "$result"
+      echo downloading zip for "$shnid"
     for link in $result; do
       # assemble URL for each .zip and download them here
-      wget --content-disposition --trust-server-names https://etreedb.org"$link" -P "$dir"
+      wget -q --content-disposition --trust-server-names https://etreedb.org"$link" -P "$dir"
     done
   fi
-
